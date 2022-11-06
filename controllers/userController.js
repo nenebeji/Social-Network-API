499de6f11703688b3e26a1dbab16942d825cc19c
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-11-06 08:53:51 +0000

    ade corrections yo user

--- controllers/userController.js
+++ controllers/userController.js
@@ -71,7 +71,7 @@ module.exports = {
     addFriend(req, res) {
         User.findOneAndUpdate(
         { _id: req.params.userId },
-        { $addToSet: { friends: req.body } },
+        { $push: { friends: req.params.friendId } },
         { runValidators: true, new: true }
         )
         .then((user) =>
@@ -85,7 +85,7 @@ module.exports = {
     removeFriend(req, res) {
         User.findOneAndUpdate(
         { _id: req.params.userId },
-        { $pull: { friends: { friendId: req.params.friendId } } },
+        { $pull: { friends: req.params.friendId } },
         { runValidators: true, new: true }
         )
         .then((user) =>
